f9be1e9647e6d35abb6ff54b7bb600ce62ee80e3
harmeliayra17 <harmeliarahmatika17@gmail.com>
2024-03-29 04:56:03 +0800

    Praktikum-2 H071231079

---
+++ H071231079/Praktikum-2/TP2_3_H071231079.java
@@ -0,0 +1,41 @@
+package TP2;
+import java.util.Scanner;
+
+class Cuboid {
+    double height;
+    double widht;
+    double length;
+
+    double getVolume() {
+        double volume = height * widht * length;
+        return volume;
+    }
+}
+
+public class TP2_3_H071231079 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Cuboid cuboid = new Cuboid();
+
+        // cuboid.height = 16;
+        // cuboid.widht = 12;
+        // cuboid.length = 20;
+
+        try {
+            System.out.print("Masukkan tinggi kubus  : ");
+            cuboid.height = scan.nextDouble();
+
+            System.out.print("Masukkan lebar kubus   : ");
+            cuboid.widht = scan.nextDouble();
+
+            System.out.print("Masukkan panjang kubus : ");
+            cuboid.length = scan.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Input tidak valid.");
+        } finally {
+            scan.close();
+        }
+    
+        System.out.println(String.format("Volume \t\t\t: %.2f", cuboid.getVolume()));
+    }
+}
